fa4d45789719b1b0c13ba90e5cfe757cf0128567
Si-Ki <vineetkr.shr@gmail.com>
2021-12-28 10:46:12 +0530

    stat updates

--- config.h
+++ config.h
@@ -13,7 +13,7 @@ static unsigned int gappiv    = 1;        /* vert inner gap between windows */
 static unsigned int gappoh    = 1;        /* horiz outer gap between windows and screen edge */
 static unsigned int gappov    = 1;        /* vert outer gap between windows and screen edge */
 static int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
 static char *fonts[]          = { "SourceCodePro-Regular:size=10","JoyPixels:pixelsize=13:antialias=true:autohint=true", "Font Awesome 5 Brands-Regular-400:pixelsize=13:antialias=true:autohint=true", "Font Awesome 5 Free-Regular-400:pixelsize=13:antialias=true:autohint=true", "Font Awesome 5 Free-Solid-900:pixelsize=13:antialias=true:autohint=true" };
